6423842257911d3ce491c99f2f8ce1b5c199126d
Brett Heckman <brett.heckman@urbanairship.com>
2017-11-10 05:05:37 -0800

    feat: add WIP search

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,32 @@
 import React, {Component} from 'react'
+import * as BooksAPI from './BooksAPI'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
+import { Link } from 'react-router-dom'
+
+// import Book from './Book'
 
 class Search extends Component {
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query:query.trim() })
+  }
+
+  componentDidMount() {
+    BooksAPI.search(this.state.query)
+  }
+
   render() {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+          <Link
+            to="/"
+            className="close-search"
+            >Close</Link>
           <div className="search-books-input-wrapper">
             {/*
               NOTES: The search from BooksAPI is limited to a particular set of search terms.
@@ -15,14 +36,21 @@ class Search extends Component {
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input type="text" placeholder="Search by title or author"/>
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.query}
+              onChange={(event) => this.updateQuery(event.target.value)}
+            />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+          </ol>
         </div>
       </div>
     )
   }
 }
+
 export default Search
